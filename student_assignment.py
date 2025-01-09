0ded4c88f41932815e3bd5bed2ee94b18585c0ff
guoliang_lin <guoliang_lin@asus.com>
2025-01-09 15:30:10 +0800

    hw01 - 01

--- student_assignment.py
+++ student_assignment.py
@@ -5,36 +5,65 @@ from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import JsonOutputParser
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+
 def generate_hw01(question):
-    pass
-    
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+
+    message = HumanMessage(
+        content=f"請回答以下問題並以 JSON 格式輸出，格式如下: {{\"Result\": [{{\"date\": \"YYYY-MM-DD\", \"name\": \"紀念日名稱\"}}]}}: {question}"
+    )
+
+    response = llm.invoke([message])
+
+    parser = JsonOutputParser()
+    try:
+        parsed_result = parser.parse(response.content)
+    except Exception:
+        return {"json parse error"}
+
+    if "Result" in parsed_result and isinstance(parsed_result["Result"], list):
+        return parsed_result
+    else:
+        return {"json not contain Result"}
+
 def generate_hw02(question):
     pass
-    
+
+
 def generate_hw03(question2, question3):
     pass
-    
+
+
 def generate_hw04(question):
     pass
-    
+
+
 def demo(question):
     llm = AzureChatOpenAI(
-            model=gpt_config['model_name'],
-            deployment_name=gpt_config['deployment_name'],
-            openai_api_key=gpt_config['api_key'],
-            openai_api_version=gpt_config['api_version'],
-            azure_endpoint=gpt_config['api_base'],
-            temperature=gpt_config['temperature']
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
     )
     message = HumanMessage(
-            content=[
-                {"type": "text", "text": question},
-            ]
+        content=[
+            {"type": "text", "text": question},
+        ]
     )
     response = llm.invoke([message])
-    
+
     return response
