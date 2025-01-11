72620cae2e6f914c41a0ef87aa5829ac5a43a8a8
guoliang_lin <guoliang_lin@asus.com>
2025-01-11 23:42:29 +0800

    hw03 - 01

--- student_assignment.py
+++ student_assignment.py
@@ -9,6 +9,11 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.tools import tool
 import requests
 
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
@@ -60,7 +65,8 @@ def fetch_holidays_from_api(conutry, year, month, language) -> str:
             "language": language,
         }
 
-        response = requests.get("https://calendarific.com/api/v2/holidays", params=params)
+        response = requests.get(
+            "https://calendarific.com/api/v2/holidays", params=params)
         if response.status_code == 200:
             data = response.json()
 
@@ -79,6 +85,7 @@ def fetch_holidays_from_api(conutry, year, month, language) -> str:
         traceback.print_exc()
         return {"error": str(e)}
 
+
 def generate_hw02(question):
     tools = [fetch_holidays_from_api]
     llm_with_tools = AzureChatOpenAI(
@@ -95,14 +102,59 @@ def generate_hw02(question):
     ai_msg = llm_with_tools.invoke(messages)
 
     for tool_call in ai_msg.tool_calls:
-        selected_tool = {"fetch_holidays_from_api": fetch_holidays_from_api}[tool_call["name"].lower()]
+        selected_tool = {"fetch_holidays_from_api": fetch_holidays_from_api}[
+            tool_call["name"].lower()]
         tool_output = selected_tool.invoke(tool_call["args"])
-    
+
     return tool_output
 
 
 def generate_hw03(question2, question3):
-    pass
+    print("generate_hw03")
+    store = {}
+
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+    tools = [fetch_holidays_from_api]
+    llm_with_tools = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+    prompt = ChatPromptTemplate.from_messages(
+    [
+        MessagesPlaceholder(variable_name='history'),
+        ('human', '{question}'),
+    ]
+)
+    
+    
+    chain = (prompt | llm_with_tools)
+
+    agent_with_chat_history = RunnableWithMessageHistory(
+        chain,
+        get_session_history=get_session_history,
+        input_messages_key="question",
+        history_messages_key="history"
+    )
+
+    response1 = agent_with_chat_history.invoke(
+        {"question": HumanMessage(question2)},
+        config={"configurable": {"session_id": "abc123"}},
+    )
+    
+    response2 = agent_with_chat_history.invoke(
+        {"question": HumanMessage(question3)},
+        config={"configurable": {"session_id": "abc123"}},
+    )
+    
+    print(response1)
 
 
 def generate_hw04(question):
@@ -129,5 +181,5 @@ def demo(question):
 
 
 if __name__ == '__main__':
-    result = generate_hw02("2024年台灣10月紀念日有哪些?")
+    result = generate_hw03("2024年台灣10月紀念日有哪些?", "蔣公誕辰紀念日是否在這個月份?")
     print(result)
